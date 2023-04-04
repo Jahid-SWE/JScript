84d25fd762e30c14f6b3c541293594c4bb96622b
MD. Jahid Hassan <33800850+Jahid-SWE@users.noreply.github.com>
2023-04-04 16:48:47 +0600

    Arithmetic Operators

---
+++ operator/script.js
@@ -0,0 +1,16 @@
+
+//Operators
+
+
+var numberOne   = 50;
+var numbertwo   = 20;
+var adition = numberOne + numbertwo;
+var subtraction = numberOne - numbertwo;
+var multiplication = numberOne * numbertwo;
+var division = numberOne / numbertwo;
+var modulus = numberOne % numbertwo;
+document.getElementById('arightmetic').innerHTML=adition;
+// document.getElementById('arightmetic').innerHTML=subtraction;
+// document.getElementById('arightmetic').innerHTML=multiplication;
+// document.getElementById('arightmetic').innerHTML=division;
+// document.getElementById('arightmetic').innerHTML=modulus;
